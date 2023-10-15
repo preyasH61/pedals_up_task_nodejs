b07bb51a7dd2722986a88ee62f329a80e93ff4e3
preyash <preyashpipariya@gmail.com>
2023-10-15 01:34:06 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const server = express();
 
 const PORT = process.env.PORT | 4000;
 server.use(express.json());
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 5, // Max 3 requests per minute
+});
+  
+server.use(limiter);
 
 server.get("/", (req, res) => {
     res.send("api root path called")
