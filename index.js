6f0636aadb51f0eb0665e38129a8a1c7adc8ad41
Preyash <68819672+preyasH61@users.noreply.github.com>
2024-04-17 18:07:58 +0530

    Update index.js for test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT
 server.use(express.json());
 
 server.get("/", (req, res) => {
-    res.send("api root path")
+    res.send("api root path called")
 });
 
 server.get("/games", (req, res) => {
